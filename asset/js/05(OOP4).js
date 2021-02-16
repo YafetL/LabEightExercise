d0aeb83e8a1f4def60b10ada49017e000330e524
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-02-16 16:59:00 +0300

    (Sixth Commit) OOP 4 - 5 changed

--- asset/js/05(OOP4).js
+++ asset/js/05(OOP4).js
@@ -6,6 +6,9 @@ function Person(firstName, lastName) {
 
 // 1. Create a getFullName Method on prototype object 
 // 2. The Function will return firstName lastName
+Person.prototype.getFullName = function () { 
+    return `${this.firstName} ${this.lastName}`; 
+} 
 
 
 //create a customer object
@@ -13,22 +16,24 @@ function Customer(firstName, lastName, membership) {
 
     //3. Call the Person Object Constructor using call [Person.call(this,firsName,lastName)]
 
-
+    Person.call(this, firstName, lastName); 
     this.membership = membership;
 
 }
 
 // 4. Set the prototype of Customer to be Person Prototype 
 // Note : Use Object.create
-
+Customer.prototype = Object.create(Person.prototype);
 
 //5. Change the constructor of Customer to be Customer 
 
-
+Customer.prototype = new Person(); 
+Customer.prototype.constructor = Customer;
 
 // Create Customer Object pass ["Jorah", "Mormont", "PRO"]
+const custemr_1 = new Customer("Jorah", "Mormont", "PRO"); 
 
 
 //6. Display The Full Name [remove the string when you have the object]
 
-fun_proto.innerHTML = "custemr_1.getFullName()";
+fun_proto.innerHTML = custemr_1.getFullName();
