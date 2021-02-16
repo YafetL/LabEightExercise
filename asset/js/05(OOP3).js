3546a2ec4d16aaade060c3fe55329e93d01100f3
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-02-16 16:36:28 +0300

    (Fifth Commit) OOP 1 - 3 changed

--- asset/js/05(OOP3).js
+++ asset/js/05(OOP3).js
@@ -6,11 +6,22 @@
 
 
 //3. Define greeting() method that displays Full Name
+class Person_ES6{
 
+    constructor(firstName, lastName){
+
+        this.firstName = firstName; 
+        this.lastName = lastName; 
+    }
+
+    greeting() {
+        return `Hello , ${this.firstName} ${this.lastName}`
+    }
+}
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
 
-
+const Bran = new Person_ES6("Bran", "Stark");
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
